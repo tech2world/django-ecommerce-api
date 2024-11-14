1aa95e0911999375f59de575ea17df7e339a3bf4
tech2world <abdulazeezibrahim847@gmail.com>
2024-11-14 10:52:21 +0100

    added views, urls for login and register, serializers and updated settings.py

---
+++ api/serializers.py
@@ -0,0 +1,23 @@
+# api/serializers.py
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    
+    def create(self, validated_data):
+        # create a new user instance
+
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# api/views.py
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = UserSerializer
